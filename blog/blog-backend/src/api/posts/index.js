1dc6a02e028047b0b861e87d361636a9e088b9e6
mingu-Lee <kcg3798@naver.com>
2020-09-18 00:27:27 +0900

    post routing

--- blog/blog-backend/src/api/posts/index.js
+++ blog/blog-backend/src/api/posts/index.js
@@ -12,24 +12,15 @@ posts.patch('/:id', postsCtrl.update);
 export default posts;*/
 
 const Router = require('koa-router');
+const postsCtrl = require('./posts.ctrl');
+
 const posts = new Router();
 
-const printInfo = ctx => {
-    ctx.body = {
-        method: ctx.method,
-        path: ctx.path,
-        params: ctx.prams,
-    };
-}
-posts.get('/',printInfo);
-posts.post('/', printInfo);
-posts.get('/:id', printInfo);
-posts.delete('/:id', printInfo);
-posts.patch('/:id', printInfo);
-/*
 posts.get('/', postsCtrl.list);
 posts.post('/', postsCtrl.write);
 posts.get('/:id', postsCtrl.read);
 posts.delete('/:id', postsCtrl.remove);
-posts.patch('/:id', postsCtrl.update);*/
+posts.put('/:id', postsCtrl.replace);
+posts.patch('/:id', postsCtrl.update);
+
 module.exports = posts;
--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -1,4 +1,4 @@
-import Post from '../../models/post';
+/*import Post from '../../models/post';
 
 export const write = async ctx => {
     const {title, body, tags} = ctx.request.body;
@@ -30,4 +30,99 @@ export const remove = ctx => {
 };
 export const update = ctx => {
 
+};*/
+
+let postId =1;
+
+const posts = [
+    {
+        id:1,
+        title: '제목',
+        body: '내용',
+
+    },
+];
+//포스트 작성
+exports.write = ctx => {
+    const {title, body} = ctx.request.body;
+    postId += 1;
+    const post = {id: postId, title, body};
+    posts.push(post);
+    ctx.body = post;
+};
+//포스트 목록 조회
+
+exports.list = ctx => {
+    ctx.body = posts;
+};
+
+//특정 포스트 조회
+exports.read = ctx => {
+    const {id} = ctx.params;
+    //주어진 id값으로 포스트를 찾는다. 파라미터로 받아 온 값은 문자열 형식 숫자로 변환하거나 p.id값을 문자열로
+    const post = posts.find(p=> p.id.toString() == id);
+    if(!post){
+        ctx.status = 404;
+        ctx.body ={
+            message: 'post is not exist',
+        };
+        return;
+    }
+    ctx.body = post;
+};
+
+//특정 포스트 제거
+exports.remove = ctx => {
+    const {id} = ctx.params;
+    const index = posts.findIndex(p=>p.id.toString()==id);
+    //포스트가 없으면 오류 반환
+    if(index === -1){
+        ctx.status = 404;
+        ctx.body = {
+            message:'post is not exist',
+        };
+        return;
+    }
+    posts.splice(index, 1);
+    ctx.status = 204;
+
+};
+
+//포스트 수정
+exports.replace = ctx =>{
+    //put 메서드는 전체 포스트 정보를 입력하여 데이터를 통쨰로 교체할때 사용
+    const {id} = ctx.params;
+    const index = posts.findIndex(p => p.id.toString() === id);
+    if(index === -1){
+        ctx.status = 404;
+        ctx.body = {
+            message: 'post is not exist',
+        };
+        return;
+    }
+
+    //전체 객체를 덮어씌우려 객체를 새로 만들자
+    posts[index] = {
+        id,
+        ...ctx.request.body,
+    };
+    ctx.body = posts[index];
+};
+
+exports.update = ctx => {
+    const {id} = ctx.params;
+    const index = posts.findIndex(p=>p.id.toString() === id);
+    if(index === -1){
+        ctx.status = 404;
+        ctx.body ={
+            message:'post is not exist',
+        };
+        return;
+    }
+    //덮어 씌우기
+    posts[index] = {
+        ...posts[index],
+        ...ctx.request.body,
+    };
+    ctx.body = posts[index];
 };
