a583d5e4314a6c2de852f927e108cee09b07f190
mingu-Lee <kcg3798@naver.com>
2020-09-23 17:54:36 +0900

    backend upload

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -3,28 +3,19 @@
 
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+
 const api = require('./api');
 
 const app = new Koa();
+
 const router = new Router();
 
-router.get('/', ctx =>{
-    ctx.body = 'Home';
-});
 
 router.use('/api', api.routes());
+router.use(bodyParser());
 
-router.get('/about/:name?', ctx =>{
-    const {name} = ctx.params;
-    ctx.body = name ? `${name}의 소개`: 'intro';
-});
-
-router.get('/posts', ctx => {
-    const { id } = ctx.query;
-    ctx.body = id ? `post #${id}` : 'post id X';
-});
-
-app.use(router.routes()).use(router.allowedWethods());
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () =>{
     console.log('Listening to port 4000');
@@ -50,3 +41,48 @@ app.use(ctx => {
     ctx.body = 'hello';
 });
 */
+/*router.get('/', ctx =>{
+    ctx.body = 'Home';
+});
+router.get('/about/:name?', ctx =>{
+    const {name} = ctx.params;
+    ctx.body = name ? `${name}의 소개`: 'intro';
+});
+
+router.get('/posts', ctx => {
+    const { id } = ctx.query;
+    ctx.body = id ? `post #${id}` : 'post id X';
+});*/
+
+
+/*require('dotenv').config();
+const Koa  from ('koa');
+import Router from 'koa-router';
+import bodyParser from 'koa-bodyparser';
+import mongoose from 'mongoose';
+
+import api from './api';
+
+const {PORT, MONGO_URI} = process.env;
+
+mongoose
+.connect(MONGO_URI, {useNewUrlParser: true, useFindAndModify: false})
+.then(() => {
+    console.log('Connected to MongoDB');
+})
+.catch(e =>{
+    console.error(e);
+});
+
+const app=new Koa();
+const router= new Router();
+
+router.use('/api', api.routes());
+app.use(bodyParser());
+
+app.use(router.routes()).use(router.allowedMethods());
+
+const port = PORT || 4000;
+app.listen(port, () => {
+    console.log('Listening to port %d', port);
+});*/
